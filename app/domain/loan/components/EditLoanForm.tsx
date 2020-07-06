e7d5605d3480503ad2cccecee7fdf63b6e012b60
Pawel <inne.poczta@gmail.com>
2020-07-07 00:30:25 +0200

    Fixed notification time

--- app/domain/loan/components/EditLoanForm.tsx
+++ app/domain/loan/components/EditLoanForm.tsx
@@ -1,58 +1,65 @@
-import React, {useState} from "react";
-import {Picker, View, Text, StyleSheet, ScrollView} from "react-native";
-import {Library} from "../../library/types/Library";
-import {Button, Card, Divider, Input} from "react-native-elements";
-import CommonStyles from "../../../common/styles/CommonStyles";
-import {Book} from "../../book/types/Book";
-import BookListItem from "../../book/components/BookListItem";
-import {useModal} from "../../../common/components/modals/ModalHook";
-import Confirmation from "../../../common/components/modals/Confirmation";
-import OperationErrorMessageBox from "../../../common/components/messagebox/OperationErrorMessageBox";
-import BooksFinderModal from "../../book/components/books_finder/BooksFinderModal";
-import {errorDark} from "../../../common/styles/Colors";
-import {Moment} from "moment";
-import moment from "moment";
-import DateTimePickerModal from "react-native-modal-datetime-picker";
-import {EditLoanReq} from "../api/LoansApi";
+import React, { useState } from "react"
+import { Picker, View, Text, StyleSheet, ScrollView } from "react-native"
+import { Library } from "../../library/types/Library"
+import { Button, Card, Divider, Input } from "react-native-elements"
+import CommonStyles from "../../../common/styles/CommonStyles"
+import { Book } from "../../book/types/Book"
+import BookListItem from "../../book/components/BookListItem"
+import { useModal } from "../../../common/components/modals/ModalHook"
+import Confirmation from "../../../common/components/modals/Confirmation"
+import OperationErrorMessageBox from "../../../common/components/messagebox/OperationErrorMessageBox"
+import BooksFinderModal from "../../book/components/books_finder/BooksFinderModal"
+import { errorDark } from "../../../common/styles/Colors"
+import { Moment } from "moment"
+import moment from "moment"
+import DateTimePickerModal from "react-native-modal-datetime-picker"
+import { EditLoanReq } from "../api/LoansApi"
 
-export type ModifyLoanInitialData =
-    Omit<EditLoanReq, 'libraryId' | 'books'>
-    & { libraryId: string | null, books: Array<Book | null> }
+export type ModifyLoanInitialData = Omit<EditLoanReq, "libraryId" | "books"> & {
+    libraryId: string | null
+    books: Array<Book | null>
+}
 export type ModifyLoanData =
-    { library: Library, books: Array<Book | null>, type: 'CREATE' }
-    | Omit<ModifyLoanInitialData, 'libraryId'> & { library: Library, type: 'UPDATE' }
+    | { library: Library; books: Array<Book | null>; type: "CREATE" }
+    | (Omit<ModifyLoanInitialData, "libraryId"> & { library: Library; type: "UPDATE" })
 
 type Props = {
     initialData?: ModifyLoanInitialData
     availableLibraries: Library[]
     onSubmit: (data: ModifyLoanData) => void
-    submitError: boolean,
+    submitError: boolean
     processingSubmit: boolean
 }
 
-const EditLoanForm: React.FC<Props> = ({availableLibraries, onSubmit, submitError, processingSubmit, initialData}) => {
+const EditLoanForm: React.FC<Props> = ({
+    availableLibraries,
+    onSubmit,
+    submitError,
+    processingSubmit,
+    initialData,
+}) => {
     const selectCurrentLibrary = () => {
         if (availableLibraries.length < 1) {
             return undefined
         }
-        const initialLib = availableLibraries.find(l => l.id === initialData?.libraryId);
+        const initialLib = availableLibraries.find((l) => l.id === initialData?.libraryId)
         return initialLib ? initialLib : availableLibraries[0]
-    };
+    }
 
-    const [currentLibrary, setCurrentLibrary] = useState(selectCurrentLibrary());
-    const [books, setBooks] = useState<Array<Book | null>>(initialData?.books || []);
-    const [returnTo, setReturnTo] = useState<Moment | null>(initialData?.returnTo || null);
-    const [returnToModalVisible, setReturnToModalVisible] = useState(false);
-    const [booksFinderVisible, setBooksFinderVisible] = useState(false);
+    const [currentLibrary, setCurrentLibrary] = useState(selectCurrentLibrary())
+    const [books, setBooks] = useState<Array<Book | null>>(initialData?.books || [])
+    const [returnTo, setReturnTo] = useState<Moment | null>(initialData?.returnTo || null)
+    const [returnToModalVisible, setReturnToModalVisible] = useState(false)
+    const [booksFinderVisible, setBooksFinderVisible] = useState(false)
 
     const [confirmOverrun, showConfirmOverrun] = useModal(Confirmation, {
-        content: 'Przekroczono limit wypożyczeń. Kontynuować?',
-        onConfirm: () => finishEdition()
-    });
+        content: "Przekroczono limit wypożyczeń. Kontynuować?",
+        onConfirm: () => finishEdition(),
+    })
 
-    const removeBook = (index: number) => setBooks(prev => prev.filter((_, i) => i !== index));
+    const removeBook = (index: number) => setBooks((prev) => prev.filter((_, i) => i !== index))
 
-    const canSubmit = Boolean(currentLibrary) && books.length > 0 && !processingSubmit;
+    const canSubmit = Boolean(currentLibrary) && books.length > 0 && !processingSubmit
 
     const submit = () => {
         if (currentLibrary?.booksLimit && currentLibrary.booksLimit < books.length) {
@@ -60,7 +67,7 @@ const EditLoanForm: React.FC<Props> = ({availableLibraries, onSubmit, submitErro
         } else {
             finishEdition()
         }
-    };
+    }
 
     const finishEdition = () => {
         if (!currentLibrary) {
@@ -72,111 +79,115 @@ const EditLoanForm: React.FC<Props> = ({availableLibraries, onSubmit, submitErro
                 library: currentLibrary,
                 returnedAt: initialData.returnedAt,
                 returnTo: returnTo || moment(),
-                type: "UPDATE"
+                type: "UPDATE",
             })
         } else {
-            onSubmit({library: currentLibrary, books, type: 'CREATE'})
+            onSubmit({ library: currentLibrary, books, type: "CREATE" })
         }
-    };
+    }
 
     const renderBookItem = (book: Book | null, idx: number) => {
         const removeIcon = {
-            name: 'remove',
-            type: 'font-awesome',
+            name: "remove",
+            type: "font-awesome",
             onPress: () => removeBook(idx),
             size: 18,
-            color: errorDark
-        };
-        return <BookListItem book={book} key={idx} rightIcon={removeIcon}/>
-    };
+            color: errorDark,
+        }
+        return <BookListItem book={book} key={idx} rightIcon={removeIcon} />
+    }
 
     const renderReturnToSection = (date: Moment) => (
         <View>
             <DateTimePickerModal
-                onConfirm={d => {
-                    setReturnToModalVisible(false);
-                    setReturnTo(moment(d));
+                onConfirm={(d) => {
+                    setReturnToModalVisible(false)
+                    const now = moment()
+                    const currentHours = now.hours()
+                    const currentMinutes = now.minutes()
+                    const updatedMoment = moment(d).clone().add(currentHours, "hours").add(currentMinutes, "minutes")
+                    setReturnTo(moment(updatedMoment))
                 }}
                 onCancel={() => setReturnToModalVisible(false)}
                 isVisible={returnToModalVisible}
-                mode='date'
-                date={date.clone().startOf('date').toDate()}
+                mode="date"
+                date={date.clone().startOf("date").toDate()}
             />
             <Text style={CommonStyles.inputLabel}>Termin zwrotu</Text>
             <Input
-                value={date.format('dddd, D MMMM YYYY').toString()}
-                rightIcon={{type: 'antdesign', name: 'calendar', onPress: () => setReturnToModalVisible(true)}}
+                value={date.format("dddd, D MMMM YYYY").toString()}
+                rightIcon={{ type: "antdesign", name: "calendar", onPress: () => setReturnToModalVisible(true) }}
             />
         </View>
-    );
+    )
 
     return (
         <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}>
             <BooksFinderModal
                 visible={booksFinderVisible}
                 onClose={() => setBooksFinderVisible(false)}
-                onFinish={(book: Book) => setBooks(prev => [...prev, book])}
+                onFinish={(book: Book) => setBooks((prev) => [...prev, book])}
             />
             {confirmOverrun}
-            <OperationErrorMessageBox visible={submitError}/>
+            <OperationErrorMessageBox visible={submitError} />
             <Card>
                 <View style={styles.sectionContainer}>
                     <Text style={CommonStyles.inputLabel}>Biblioteka</Text>
                     <Picker
-                        mode='dropdown'
+                        mode="dropdown"
                         selectedValue={currentLibrary?.id}
-                        onValueChange={value => {
-                            setCurrentLibrary(availableLibraries.find(l => l.id === value) || undefined)
+                        onValueChange={(value) => {
+                            setCurrentLibrary(availableLibraries.find((l) => l.id === value) || undefined)
                         }}
                     >
-                        {availableLibraries.map(lib => <Picker.Item label={lib.name} value={lib.id} key={lib.id}/>)}
+                        {availableLibraries.map((lib) => (
+                            <Picker.Item label={lib.name} value={lib.id} key={lib.id} />
+                        ))}
                     </Picker>
-                    <Divider/>
+                    <Divider />
                 </View>
                 {returnTo && renderReturnToSection(returnTo)}
                 <View style={styles.sectionContainer}>
                     <Text style={CommonStyles.inputLabel}>Książki</Text>
                     <View style={styles.addButtonsContainer}>
                         <Button
-                            title='Znajdź opis i dodaj'
+                            title="Znajdź opis i dodaj"
                             containerStyle={styles.addBookButtonContainer}
                             titleStyle={styles.addBookButtonTitle}
                             onPress={() => setBooksFinderVisible(true)}
                         />
-                        <View style={styles.addBookSeparator}/>
+                        <View style={styles.addBookSeparator} />
                         <Button
-                            title='Dodaj bez opisu'
+                            title="Dodaj bez opisu"
                             containerStyle={styles.addBookButtonContainer}
                             titleStyle={styles.addBookButtonTitle}
-                            onPress={() => setBooks(prev => [...prev, null])}
+                            onPress={() => setBooks((prev) => [...prev, null])}
                         />
                     </View>
-                    <View>
-                        {books.map((book, idx) => renderBookItem(book, idx))}
-                    </View>
+                    <View>{books.map((book, idx) => renderBookItem(book, idx))}</View>
                 </View>
-                <Button title='Zapisz' disabled={!canSubmit} onPress={submit} loading={processingSubmit}/>
+                <Button title="Zapisz" disabled={!canSubmit} onPress={submit} loading={processingSubmit} />
             </Card>
         </ScrollView>
     )
-};
+}
 
 const styles = StyleSheet.create({
     sectionContainer: {
-        marginVertical: 10
+        marginVertical: 10,
     },
     addButtonsContainer: {
-        flexDirection: 'row'
+        flexDirection: "row",
     },
     addBookButtonContainer: {
-        flex: 2
+        flex: 2,
     },
     addBookButtonTitle: {
-        fontSize: 12
+        fontSize: 12,
     },
     addBookSeparator: {
-        flex: 1
-    }
-});
+        flex: 1,
+    },
+})
 
 export default EditLoanForm
--- app/domain/loan/utils/notificationsRepositoryHelpers.ts
+++ app/domain/loan/utils/notificationsRepositoryHelpers.ts
@@ -1,58 +1,91 @@
-import {Action, Success} from "typescript-fsa";
-import {Loan} from "../types/Loan";
-import moment from "moment";
-import AsyncStorage from "@react-native-community/async-storage";
-import findKey from "lodash/findKey";
-import omit from "lodash/omit";
-import fromPairs from "lodash/fromPairs";
-import PushNotification from "react-native-push-notification";
-import {brandDark} from "../../../common/styles/Colors";
-import {AppState} from "../../../common/store";
+import { Loan } from "./../types/Loan"
+import { Action, Success } from "typescript-fsa"
+import moment from "moment"
+import AsyncStorage from "@react-native-community/async-storage"
+import findKey from "lodash/findKey"
+import omit from "lodash/omit"
+import pickBy from "lodash/pickBy"
+import fromPairs from "lodash/fromPairs"
+import PushNotification from "react-native-push-notification"
+import { brandDark } from "../../../common/styles/Colors"
+import { AppState } from "../../../common/store"
 
-const LOAN_NOTIFICATIONS_REPOSITORY_STORAGE_KEY = 'loanNotifications'
-type LoanNotificationRepositoryEntry = { id: string, version: string, returnTo: number }
+const LOAN_NOTIFICATIONS_REPOSITORY_STORAGE_KEY = "loanNotifications"
+type LoanNotificationRepositoryEntry = { id: string; version: string; returnTo: number }
 
 export const updateLoanNotificationsOnAction = (action: Action<Success<void, Loan[]>>, appState?: AppState) => {
-    AsyncStorage.getItem(LOAN_NOTIFICATIONS_REPOSITORY_STORAGE_KEY).then(maybeNotifications => {
-        const registered: Record<string, LoanNotificationRepositoryEntry> = maybeNotifications ? JSON.parse(maybeNotifications) : {}
-        const newLoans = action.payload.result.filter(loan => !Object.values(registered).map(entry => entry.id).includes(loan.id))
-        const removedLoans = Object.keys(registered).filter(notificationId => !action.payload.result.map(loan => loan.id).includes(registered[notificationId].id))
-        const changedLoans = action.payload.result.filter(loan => findKey(registered, entry => entry.id === loan.id && entry.version !== loan.version))
+    AsyncStorage.getItem(LOAN_NOTIFICATIONS_REPOSITORY_STORAGE_KEY).then((maybeNotifications) => {
+        const registered: Record<string, LoanNotificationRepositoryEntry> = maybeNotifications
+            ? JSON.parse(maybeNotifications)
+            : {}
+        const newLoans = action.payload.result.filter(
+            (loan) =>
+                !Object.values(registered)
+                    .map((entry) => entry.id)
+                    .includes(loan.id)
+        )
 
-        const changedNotificationIds = changedLoans
-            .map(loan => findKey(registered, entry => entry.id === loan.id))
-            .filter((id): id is string => id !== undefined)
+        const removedLoans = Object.keys(registered).filter(
+            (notificationId) => !action.payload.result.map((loan) => loan.id).includes(registered[notificationId].id)
+        )
+
+        const changedLoans = action.payload.result.filter((loan) =>
+            findKey(registered, (entry) => entry.id === loan.id && entry.version !== loan.version)
+        )
+
+        const changedNotificationIds: string[] = flatMap(
+            changedLoans.map((loan) => Object.keys(pickBy(registered, (v) => v.id === loan.id)))
+        )
 
         const toRemove = removedLoans.concat(changedNotificationIds)
-        toRemove.forEach(notificationId => PushNotification.cancelLocalNotifications({id: notificationId}))
+        toRemove.forEach((notificationId) => PushNotification.cancelLocalNotifications({ id: notificationId }))
         const registeredWithoutRemoved: Record<string, LoanNotificationRepositoryEntry> = omit(registered, toRemove)
 
-        const newEntries = fromPairs(newLoans.concat(changedLoans)
-            .map(loan => {
-                const maybeLibraryName = appState?.libraries.librariesStatus.data?.find(l => l.id === loan.libraryId)?.name
-                const notificationId = createNotification(loan.returnTo.unix(), maybeLibraryName)
-                const entry = {id: loan.id, returnTo: loan.returnTo.unix(), version: loan.version}
-                return [notificationId, entry]
-            }))
+        const notificationsForLoan: (l: Loan) => Array<[string, LoanNotificationRepositoryEntry]> = (loan) => {
+            const maybeLibraryName = appState?.libraries.librariesStatus.data?.find((l) => l.id === loan.libraryId)
+                ?.name
+            return [7, 3, 1].map((days) => createNotificationBefore(days, loan, maybeLibraryName))
+        }
 
-        const updatedRegistry: Record<string, LoanNotificationRepositoryEntry> = Object.assign({}, registeredWithoutRemoved, newEntries)
+        const createNotificationBefore = (daysBefore: number, loan: Loan, libraryName?: string) => {
+            const notificationId = createNotification(daysBefore, loan.returnTo.unix(), libraryName)
+            const entry = {
+                id: loan.id,
+                returnTo: loan.returnTo.unix(),
+                version: loan.version,
+            }
+            const pair: [string, LoanNotificationRepositoryEntry] = [notificationId, entry]
+            return pair
+        }
+
+        const newEntries = fromPairs(flatMap(newLoans.concat(changedLoans).map(notificationsForLoan)))
+
+        const updatedRegistry: Record<string, LoanNotificationRepositoryEntry> = Object.assign(
+            {},
+            registeredWithoutRemoved,
+            newEntries
+        )
         return AsyncStorage.setItem(LOAN_NOTIFICATIONS_REPOSITORY_STORAGE_KEY, JSON.stringify(updatedRegistry))
     })
 }
 
-const createNotification = (returnTo: number, libraryName?: string | null) => {
+function flatMap<T>(list: Array<Array<T>>) {
+    return ([] as Array<T>).concat(...list)
+}
+
+const createNotification = (daysBefore: number, returnTo: number, libraryName?: string | null) => {
     const notificationId = (moment().unix() + Math.floor(Math.random() * 10000)).toString(10)
     const returnToMoment = moment(returnTo * 1000)
-    const messageBase = returnToMoment.isAfter(moment()) ? 'Zbliża się termin zwrotu.' : 'Minął termin zwrotu.'
-    const message = libraryName ? messageBase + ' Biblioteka ' + libraryName : messageBase
-    const threeDaysBeforeEnd = returnToMoment.clone().subtract(3, 'days')
-    const date = threeDaysBeforeEnd.isAfter(moment()) ? threeDaysBeforeEnd.toDate() : moment().clone().add(1, 'hour').toDate()
+    const messageBase = returnToMoment.isAfter(moment()) ? "Zbliża się termin zwrotu." : "Minął termin zwrotu."
+    const message = libraryName ? messageBase + " Biblioteka " + libraryName : messageBase
+    const daysBeforeEnd = returnToMoment.clone().subtract(daysBefore, "days")
+    const date = daysBeforeEnd.isAfter(moment()) ? daysBeforeEnd.toDate() : moment().clone().add(1, "hour").toDate()
 
     PushNotification.localNotificationSchedule({
         id: notificationId,
         color: brandDark,
         message,
-        date
+        date,
     })
     return notificationId
 }
